bbff32945b23925a7869fe0f1c1c1ca3af8f3218
Fabricio <fabricioalvesfragoso@gmail.com>
2024-09-09 20:13:48 -0300

    ListaDeTarefa

---
+++ src/main/java/com/mycompany/listatarefas/ListaTarefas.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.listatarefas;
+
+/**
+ *
+ * @author Fabrício
+ */
+public class ListaTarefas {
+
+    public static void main(String[] args) {
+        Tela form = new Tela();    
+      form.setVisible(true);
+    }
+}
